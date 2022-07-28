622f0cb903bca6a60babb41ecdc1556840b074f8
Andre Weissflog <floooh@gmail.com>
2022-07-28 17:13:17 +0200

    build_clibs_macos.sh: build x86_64 libraries

--- build_clibs_macos.sh
+++ build_clibs_macos.sh
@@ -1,35 +1,78 @@
 set -e
 
-build_lib_release() {
+build_lib_arm64_release() {
     src=$1
     dst=$2
     backend=$3
-    MACOSX_DEPLOYMENT_TARGET=10.13 clang -c -O2 -x objective-c -DNDEBUG -DIMPL -D$backend c/$src.c
+    echo $dst
+    MACOSX_DEPLOYMENT_TARGET=10.13 clang -c -O2 -x objective-c -arch arm64 -DNDEBUG -DIMPL -D$backend c/$src.c
     ar rcs $dst.a $src.o
 }
 
-build_lib_debug() {
+build_lib_arm64_debug() {
     src=$1
     dst=$2
     backend=$3
-    MACOSX_DEPLOYMENT_TARGET=10.13 clang -c -g -x objective-c -DIMPL -D$backend c/$src.c
+    echo $dst
+    MACOSX_DEPLOYMENT_TARGET=10.13 clang -c -g -x objective-c -arch arm64 -DIMPL -D$backend c/$src.c
     ar rcs $dst.a $src.o
 }
 
-build_lib_release sokol_gfx         sokol/gfx/sokol_gfx_macos_arm64_metal_release SOKOL_METAL
-build_lib_release sokol_app         sokol/app/sokol_app_macos_arm64_metal_release SOKOL_METAL
-build_lib_release sokol_glue        sokol/glue/sokol_glue_macos_arm64_metal_release SOKOL_METAL
-build_lib_release sokol_audio       sokol/audio/sokol_audio_macos_arm64_metal_release SOKOL_METAL
-build_lib_release sokol_debugtext   sokol/debugtext/sokol_debugtext_macos_arm64_metal_release SOKOL_METAL
-build_lib_release sokol_shape       sokol/shape/sokol_shape_macos_arm64_metal_release SOKOL_METAL
-build_lib_release sokol_gl          sokol/gl/sokol_gl_macos_arm64_metal_release SOKOL_METAL
+build_lib_x64_release() {
+    src=$1
+    dst=$2
+    backend=$3
+    echo $dst
+    MACOSX_DEPLOYMENT_TARGET=10.13 clang -c -O2 -x objective-c -arch x86_64 -DNDEBUG -DIMPL -D$backend c/$src.c
+    ar rcs $dst.a $src.o
+}
 
-build_lib_debug sokol_gfx           sokol/gfx/sokol_gfx_macos_arm64_metal_debug SOKOL_METAL
-build_lib_debug sokol_app           sokol/app/sokol_app_macos_arm64_metal_debug SOKOL_METAL
-build_lib_debug sokol_glue          sokol/glue/sokol_glue_macos_arm64_metal_debug SOKOL_METAL
-build_lib_debug sokol_audio         sokol/audio/sokol_audio_macos_arm64_metal_debug SOKOL_METAL
-build_lib_debug sokol_debugtext     sokol/debugtext/sokol_debugtext_macos_arm64_metal_debug SOKOL_METAL
-build_lib_debug sokol_shape         sokol/shape/sokol_shape_macos_arm64_metal_debug SOKOL_METAL
-build_lib_debug sokol_gl            sokol/gl/sokol_gl_macos_arm64_metal_debug SOKOL_METAL
+build_lib_x64_debug() {
+    src=$1
+    dst=$2
+    backend=$3
+    echo $dst
+    MACOSX_DEPLOYMENT_TARGET=10.13 clang -c -g -x objective-c -arch x86_64 -DIMPL -D$backend c/$src.c
+    ar rcs $dst.a $src.o
+}
 
+# ARM + Metal + Release
+build_lib_arm64_release sokol_gfx         sokol/gfx/sokol_gfx_macos_arm64_metal_release SOKOL_METAL
+build_lib_arm64_release sokol_app         sokol/app/sokol_app_macos_arm64_metal_release SOKOL_METAL
+build_lib_arm64_release sokol_glue        sokol/glue/sokol_glue_macos_arm64_metal_release SOKOL_METAL
+build_lib_arm64_release sokol_time        sokol/time/sokol_time_macos_arm64_metal_release SOKOL_METAL
+build_lib_arm64_release sokol_audio       sokol/audio/sokol_audio_macos_arm64_metal_release SOKOL_METAL
+build_lib_arm64_release sokol_debugtext   sokol/debugtext/sokol_debugtext_macos_arm64_metal_release SOKOL_METAL
+build_lib_arm64_release sokol_shape       sokol/shape/sokol_shape_macos_arm64_metal_release SOKOL_METAL
+build_lib_arm64_release sokol_gl          sokol/gl/sokol_gl_macos_arm64_metal_release SOKOL_METAL
+
+# ARM + Metal + Debug
+build_lib_arm64_debug sokol_gfx           sokol/gfx/sokol_gfx_macos_arm64_metal_debug SOKOL_METAL
+build_lib_arm64_debug sokol_app           sokol/app/sokol_app_macos_arm64_metal_debug SOKOL_METAL
+build_lib_arm64_debug sokol_glue          sokol/glue/sokol_glue_macos_arm64_metal_debug SOKOL_METAL
+build_lib_arm64_debug sokol_time          sokol/time/sokol_time_macos_arm64_metal_debug SOKOL_METAL
+build_lib_arm64_debug sokol_audio         sokol/audio/sokol_audio_macos_arm64_metal_debug SOKOL_METAL
+build_lib_arm64_debug sokol_debugtext     sokol/debugtext/sokol_debugtext_macos_arm64_metal_debug SOKOL_METAL
+build_lib_arm64_debug sokol_shape         sokol/shape/sokol_shape_macos_arm64_metal_debug SOKOL_METAL
+build_lib_arm64_debug sokol_gl            sokol/gl/sokol_gl_macos_arm64_metal_debug SOKOL_METAL
+
+# x64 + Metal + Release
+build_lib_x64_release sokol_gfx         sokol/gfx/sokol_gfx_macos_x64_metal_release SOKOL_METAL
+build_lib_x64_release sokol_app         sokol/app/sokol_app_macos_x64_metal_release SOKOL_METAL
+build_lib_x64_release sokol_glue        sokol/glue/sokol_glue_macos_x64_metal_release SOKOL_METAL
+build_lib_x64_release sokol_time        sokol/time/sokol_time_macos_x64_metal_release SOKOL_METAL
+build_lib_x64_release sokol_audio       sokol/audio/sokol_audio_macos_x64_metal_release SOKOL_METAL
+build_lib_x64_release sokol_debugtext   sokol/debugtext/sokol_debugtext_macos_x64_metal_release SOKOL_METAL
+build_lib_x64_release sokol_shape       sokol/shape/sokol_shape_macos_x64_metal_release SOKOL_METAL
+build_lib_x64_release sokol_gl          sokol/gl/sokol_gl_macos_x64_metal_release SOKOL_METAL
+
+# x64 + Metal + Debug
+build_lib_x64_debug sokol_gfx           sokol/gfx/sokol_gfx_macos_x64_metal_debug SOKOL_METAL
+build_lib_x64_debug sokol_app           sokol/app/sokol_app_macos_x64_metal_debug SOKOL_METAL
+build_lib_x64_debug sokol_glue          sokol/glue/sokol_glue_macos_x64_metal_debug SOKOL_METAL
+build_lib_x64_debug sokol_time          sokol/time/sokol_time_macos_x64_metal_debug SOKOL_METAL
+build_lib_x64_debug sokol_audio         sokol/audio/sokol_audio_macos_x64_metal_debug SOKOL_METAL
+build_lib_x64_debug sokol_debugtext     sokol/debugtext/sokol_debugtext_macos_x64_metal_debug SOKOL_METAL
+build_lib_x64_debug sokol_shape         sokol/shape/sokol_shape_macos_x64_metal_debug SOKOL_METAL
+build_lib_x64_debug sokol_gl            sokol/gl/sokol_gl_macos_x64_metal_debug SOKOL_METAL
 rm *.o
