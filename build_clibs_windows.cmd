0f372f70e1228a8e07925244bf7e27d80329bca6
jakubtomsu <66876057+jakubtomsu@users.noreply.github.com>
2024-04-10 12:42:04 +0200

    change prefix to sokol_dll_* libs

--- build_clibs_windows.cmd
+++ build_clibs_windows.cmd
@@ -19,13 +19,13 @@ lib /OUT:sokol\sokol_windows_x64_gl_release.lib sokol.obj
 del sokol.obj
 
 echo === D3D11 Debug DLL ===
-cl /OUT:sokol\sokol_windows_x64_d3d11_debug.dll /D_DEBUG /DIMPL /DSOKOL_D3D11 c\sokol_dll.c /Z7 /LDd /MDd /DLL
+cl /OUT:sokol\sokol_dll_windows_x64_d3d11_debug.dll /D_DEBUG /DIMPL /DSOKOL_D3D11 c\sokol_dll.c /Z7 /LDd /MDd /DLL
 
 echo === D3D11 Release DLL ===
-cl /OUT:sokol\sokol_windows_x64_d3d11_release.dll /D_DEBUG /DIMPL /DSOKOL_D3D11 c\sokol_dll.c /LD /MD /DLL
+cl /OUT:sokol\sokol_dll_windows_x64_d3d11_release.dll /D_DEBUG /DIMPL /DSOKOL_D3D11 c\sokol_dll.c /LD /MD /DLL
 
 echo === GL Debug DLL ===
-cl /OUT:sokol\sokol_windows_x64_gl_debug.dll /D_DEBUG /DIMPL /DSOKOL_GLCORE33 c\sokol_dll.c /Z7 /LDd /MDd /DLL
+cl /OUT:sokol\sokol_dll_windows_x64_gl_debug.dll /D_DEBUG /DIMPL /DSOKOL_GLCORE33 c\sokol_dll.c /Z7 /LDd /MDd /DLL
 
 echo === GL Release DLL ===
-cl /OUT:sokol\sokol_windows_x64_gl_release.dll /D_DEBUG /DIMPL /DSOKOL_GLCORE33 c\sokol_dll.c /LD /MD /DLL
+cl /OUT:sokol\sokol_dll_windows_x64_gl_release.dll /D_DEBUG /DIMPL /DSOKOL_GLCORE33 c\sokol_dll.c /LD /MD /DLL
