a59126c673ee70bd0ffdde37cfab6244d7396b2a
Karl Zylinski <karl@zylinski.se>
2025-01-27 01:55:02 +0100

    Use .a instead of .lib when generating wasm sokol libs

--- sokol/build_clibs_wasm.bat
+++ sokol/build_clibs_wasm.bat
@@ -4,16 +4,16 @@ set sources=log app gfx glue time audio debugtext shape gl
 
 REM Debug
 for %%s in (%sources%) do (
-	echo %%s\sokol_%%s_wasm_gl_debug.lib
+    echo %%s\sokol_%%s_wasm_gl_debug.a
     call emcc -c -g -DIMPL -DSOKOL_GLES3 c\sokol_%%s.c
-    call emar rcs %%s\sokol_%%s_wasm_gl_debug.lib sokol_%%s.o
+    call emar rcs %%s\sokol_%%s_wasm_gl_debug.a sokol_%%s.o
     del sokol_%%s.o
 )
 
 REM Release
 for %%s in (%sources%) do (
-	echo %%s\sokol_%%s_wasm_gl_release.lib
+    echo %%s\sokol_%%s_wasm_gl_release.a
     call emcc -c -O2 -DNDEBUG -DIMPL -DSOKOL_GLES3 c\sokol_%%s.c
-    call emar rcs %%s\sokol_%%s_wasm_gl_release.lib sokol_%%s.o
+    call emar rcs %%s\sokol_%%s_wasm_gl_release.a sokol_%%s.o
     del sokol_%%s.o
 )
